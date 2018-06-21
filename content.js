9af19beebfcbfed5948295f385b823a30747e077
embbnux <embbnux@embbnux.com>
2018-06-21 15:15:11 +0800

    support to open office calendar without refreshing page

--- content.js
+++ content.js
@@ -7,6 +7,34 @@ console.log('import RingCentral Embeddable Voice to web page');
   rcs0.parentNode.insertBefore(rcs, rcs0);
 })();
 
+function buildO365CalendarUrl(params={}) {
+  var encoded = [];
+  if ('subject' in params) {
+    encoded.push('subject=' + encodeURIComponent(params['subject']));
+  }
+  if ('body' in params) {
+    encoded.push('body=' + encodeURIComponent(params['body']));
+  }
+  encoded.push('path=/calendar/action/compose')
+  return 'https://outlook.office.com/owa/#' + encoded.join('&');
+}
+
+function responseMessage(request, response) {
+  document.querySelector("#rc-widget-adapter-frame").contentWindow.postMessage({
+    type: 'rc-post-message-response',
+    responseId: request.requestId,
+    response,
+  }, '*');
+}
+
+function inviteConference(request) {
+  const calendarUrl = buildO365CalendarUrl({
+    'subject':'New Conference',
+    'body': request.body.conference.inviteText});
+  window.location.href = calendarUrl;
+  responseMessage(request, { data: 'ok' });
+}
+
 // Interact with RingCentral Embeddable Voice:
 window.addEventListener('message', (e) => {
   const data = e.data;
@@ -24,6 +52,10 @@ window.addEventListener('message', (e) => {
         // get call on start a outbound call event
         console.log('RingCentral Embeddable Voice Extension:', data.call);
         break;
+      case 'rc-post-message-request':
+        if (data.path === '/conference/invite') {
+          inviteConference(data);
+        }
       default:
         break;
     }
@@ -50,3 +82,24 @@ chrome.runtime.onMessage.addListener(
     sendResponse('ok');
   }
 );
+
+function registerService() {
+  document.querySelector("#rc-widget-adapter-frame").contentWindow.postMessage({
+    type: 'rc-adapter-register-third-party-service',
+    service: {
+      name: 'TestService',
+      conferenceInvitePath: '/conference/invite',
+      conferenceInviteTitle: 'Invite with Office 365 Calendar'
+    }
+  }, '*');
+}
+
+var registered = false;
+window.addEventListener('message', function (e) {
+  const data = e.data;
+  if (data && data.type === 'rc-adapter-pushAdapterState' && registered === false) {
+    registered = true;
+    registerService();
+  }
+});
+
